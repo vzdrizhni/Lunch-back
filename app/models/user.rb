e2df19b9928ae4386c580626c3c60e9747c11a25
Roman <vzdrizhni@gmail.com>
2021-04-05 16:55:47 +0500

    First user admin implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
+
+    before_create :set_admin
+
+    private
+
+    def set_admin
+      self.admin = true unless User.exists?
+    end
 end

0efd277895d39fbd830e03266d9eed7567bf78b1
Roman <vzdrizhni@gmail.com>
2021-04-05 16:00:01 +0500

    Change menu item model

---
+++ db/migrate/20210405105227_rename_type_to_menu_item_type.rb
@@ -0,0 +1,7 @@
+class RenameTypeToMenuItemType < ActiveRecord::Migration[6.1]
+  def change
+    change_table :menu_items do |t|
+      t.rename :type, :menu_item_type
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_103901) do
+ActiveRecord::Schema.define(version: 2021_04_05_105227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "menu_items", force: :cascade do |t|
-    t.integer "type"
+    t.integer "menu_item_type"
     t.bigint "menu_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

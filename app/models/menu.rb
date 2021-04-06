b87d84f22d1956b14e2fe794dfccdefaa3f09792
Roman <vzdrizhni@gmail.com>
2021-04-06 12:35:04 +0500

    validate uniqueness of menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,6 @@
 class Menu < ApplicationRecord
   belongs_to :weekday
-  belongs_to :menu_item  
+  belongs_to :menu_item
+  
+  validates_uniqueness_of :weekday_id, :scope => :menu_item_id, :message => "The menu can only have one menu item of this name"
 end

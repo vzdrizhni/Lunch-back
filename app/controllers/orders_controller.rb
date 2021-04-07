9db70151e665b3ea209d7a499689c459071f24cb
Roman <vzdrizhni@gmail.com>
2021-04-07 21:02:24 +0500

    Finish orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 class OrdersController < ApplicationController
+  before_action :is_admin?, only: [:index]
+
+  def index
+    orders = Order.all
+
+    if orders
+      orders_serialized = parse_json orders
+      render json: { success: true, data: orders_serialized}
+    else
+      render json: { success: false, message: 'Something went wrong'}
+    end
+  end
+
   def update
     order = Order.find(params[:id])
 
@@ -34,7 +47,7 @@ class OrdersController < ApplicationController
     order = Order.find(params[:id])
     order.destroy
 
-    if order.nil?
+    if order
       render json: { success: true, message: "An order with #{order.id} was deleted" }
     else
       render json: { success: false, message: "Something went wrong" }

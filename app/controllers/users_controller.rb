91b79ba1882ffb812883460cf6dcc88453f60e3d
Roman <vzdrizhni@gmail.com>
2021-04-08 14:59:00 +0500

    Fix login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   end
 
   def login
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(email: params[:email])
 
     if @user&.authenticate(params[:password])
       token = encode_token({ user_id: @user.id })

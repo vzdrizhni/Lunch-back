3e0e26478e9d0a750079c568e3dbda4891e081ed
Roman <vzdrizhni@gmail.com>
2021-04-23 20:23:36 +0500

    Remove comments from weekday serializer

--- app/serializers/weekday_serializer.rb
+++ app/serializers/weekday_serializer.rb
@@ -4,8 +4,4 @@ class WeekdaySerializer < ActiveModel::Serializer
   has_many :menu_items
   has_many :menus
   has_many :orders
-
-  # def image
-  #   object.menu_items.image.service_url if object.menu_items.image.attached?
-  # end
 end
